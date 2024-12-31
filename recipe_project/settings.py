e7c6279dd3ac76c77122065a89388a3734c6d035
shareifjones <shareifjones@gmail.com>
2024-12-31 01:33:42 -0500

    updated site name

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','django-insecure-ml49cp(e)=yakpe
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'shrouded-harbor-70828-55c317b38622.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'shrouded-harbor-70828-55c317b38622.herokuapp.com', 'shareifs-recipes']
 
 
 
